548bdc21dd34fa770e0e3dc3b6701118c8999d80
Eike Kettner <eike.kettner@posteo.de>
2020-03-31 18:48:55 +0200

    Remove coursier plugin; update minitest to 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
 
   // https://github.com/monix/minitest
   // Apache 2.0
-  val minitest = "io.monix" %% "minitest" % "2.4.0"
-  val `minitest-laws` = "io.monix" %% "minitest-laws" % "2.4.0"
+  val minitest = "io.monix" %% "minitest" % "2.7.0"
+  val `minitest-laws` = "io.monix" %% "minitest-laws" % "2.7.0"
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,3 @@
-addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.1.0-M3")
 addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.9.0")
 addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "1.0.0")
 addSbtPlugin("com.jsuereth" % "sbt-pgp" % "2.0.1")
