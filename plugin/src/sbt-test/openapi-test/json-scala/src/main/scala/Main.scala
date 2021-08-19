00c54cfad0062288217a2576b83dd73897465190
Scala Steward <me@scala-steward.org>
2021-08-19 08:51:15 +0200

    Reformat with scalafmt 3.0.0

--- plugin/src/sbt-test/openapi-test/json-scala/src/main/scala/Main.scala
+++ plugin/src/sbt-test/openapi-test/json-scala/src/main/scala/Main.scala
@@ -8,14 +8,32 @@ object Main {
   def main(args: Array[String]): Unit = {
     testJson(RoomDto("main room dto", Some(68)))
     testJson(SimpleStringDto("bla bla bla"))
-    testJson(CourseDto("course10", LocalDate.now, RoomDto("heat room", Some(12)), List(PersonDto(Some("hugo"), "meyer", None))))
-    testJson(ExtractedData1Dto(Map("c1" -> CourseDto("course10", LocalDate.now, RoomDto("heat room", Some(12)), List(PersonDto(Some("hugo"), "meyer", None))))))
+    testJson(
+      CourseDto(
+        "course10",
+        LocalDate.now,
+        RoomDto("heat room", Some(12)),
+        List(PersonDto(Some("hugo"), "meyer", None))
+      )
+    )
+    testJson(
+      ExtractedData1Dto(
+        Map(
+          "c1" -> CourseDto(
+            "course10",
+            LocalDate.now,
+            RoomDto("heat room", Some(12)),
+            List(PersonDto(Some("hugo"), "meyer", None))
+          )
+        )
+      )
+    )
     testJson(ExtractedData2Dto(Map("c1" -> "v1", "c2" -> "v2")))
   }
 
   def testJson[A](a: A)(implicit d: Decoder[A], e: Encoder[A]): Unit = {
     val jsonStr = toJson(a)
-    val backA = toValue(jsonStr)
+    val backA   = toValue(jsonStr)
     println(s"JSON: $jsonStr  BACK: $backA")
     assert(backA == a)
   }
