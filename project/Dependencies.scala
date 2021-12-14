2baeab5665ed24dd2cfa4ee85f4957c608c1a71e
Scala Steward <me@scala-steward.org>
2021-12-14 14:29:08 +0100

    Update logback-classic to 1.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
 
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.26"
 
-  val `logback-classic` = "ch.qos.logback" % "logback-classic" % "1.2.7"
+  val `logback-classic` = "ch.qos.logback" % "logback-classic" % "1.2.8"
 
   // https://github.com/monix/minitest
   // Apache 2.0
