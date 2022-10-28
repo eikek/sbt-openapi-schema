96e9692d83f7ce6c0ea94e5e6a47bc8df1517226
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2022-10-28 06:15:56 +0000

    Update swagger-parser to 2.1.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val `swagger-parser` = "io.swagger.parser.v3" % "swagger-parser" % "2.1.6"
+  val `swagger-parser` = "io.swagger.parser.v3" % "swagger-parser" % "2.1.7"
 
   val swaggerCodegen = "io.swagger.codegen.v3" % "swagger-codegen-cli" % "3.0.35"
 
