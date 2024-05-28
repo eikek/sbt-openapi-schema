ab009edf3ae0d32caf827f6044856fa0046d53fa
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2024-05-28 06:11:23 +0000

    Update swagger-codegen-cli to 3.0.57

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val munitVersion = "1.0.0"
     val munitCatsEffectVersion = "2.0.0"
     val swaggerParser = "2.1.22"
-    val swaggerCodegen = "3.0.56"
+    val swaggerCodegen = "3.0.57"
   }
 
   val munit = Seq(
